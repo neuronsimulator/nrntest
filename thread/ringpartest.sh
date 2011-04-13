89b166155af12558384df02ee8943a7ce2086360
Michael Hines <michael.hines@yale.edu>
2011-04-13 09:53:50 -0400

    global variable time step works for threads + (multisplit or gap junctions), also for mpi + (multisplit or gap junctions). Does not work for mpi+threads due to lack of an appropriate NVector implementation. Also does not work for mpi + (multisplit or gap junctions) when several kinds of events (e.g. NetParEvent) are involved.

---
+++ thread/ringpartest.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+function run() {
+	mpiexec -n $1 nrniv -nobanner -mpi -c nt=$2 -c cv=$3 ringpar.hoc
+	sortspike out.dat temp
+	diff ringpar_$3.dat temp
+}
+
+function series() {
+	run 1 1 $1
+	run 1 4 $1
+	run 4 1 $1
+	run 2 2 $1
+}
+
+# fixed step
+series 0
+
+#global variable
+series 1
+
+#local variable
+series 2
+
