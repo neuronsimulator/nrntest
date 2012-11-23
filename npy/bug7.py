3caf02e5f5548c802a1103ec3228e0bc6af819e2
Michael Hines <michael.hines@yale.edu>
2012-11-23 08:54:46 -0500

    more

---
+++ npy/bug7.py
@@ -0,0 +1,9 @@
+from neuron import h
+from e import e
+
+v1 = h.Vector(5).indgen()
+v2 = v1.c #left out () so v2 is a callable for the clone function of v1
+v1.resize(10)
+print v2
+print v2.hname()
+e('print v2.size()') # should give error instead of size of v1
--- npy/e.py
+++ npy/e.py
@@ -1,9 +1,9 @@
-from __main__ import *
+import __main__
 import sys
 def e(stmt) :
   try:
     print stmt
-    exec(stmt)
+    exec stmt in __main__.__dict__
   except:
     print sys.exc_info()[0], ': ', sys.exc_info()[1]
 
