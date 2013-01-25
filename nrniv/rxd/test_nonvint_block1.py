0b8dafbb0fe0a52b9b3675bc6f924b9c82a66e00
Michael Hines <michael.hines@yale.edu>
2013-01-24 21:07:53 -0500

    decent test of nonvint block which compares result with standard hh.

--- nrniv/rxd/test_nonvint_block1.py
+++ nrniv/rxd/test_nonvint_block1.py
@@ -18,60 +18,62 @@ class kchan():
     self.call = [self.setup, self.initialize,
       self.current, self.conductance, self.fixed_step_solve,
       self.ode_count, self.ode_reinit, self.ode_fun, self.ode_solve ]
+    self.gkbar = .036
     nbs.register(self.call)
 
   def __del__(self):
     nbs.unregister(self.call)
 
-  def setup(self, unused1, unused2):
+  def setup(self):
     self.nodeindex = 1 #nbs.nodeindex(self.seg)
 
-  def initialize(self, unused1, unused2):
+  def initialize(self):
     self.n, x = self.ninftau(self.seg.v)
     #print "initialize v=%g n=%g"% (self.seg.v, self.n)
 
-  def current(self, rhs, unused):
+  def current(self, rhs):
     #print "enter current ", rhs
     v = self.seg.v
     n = self.n
-    self.g = 0.036*n*n*n*n
+    self.g = self.gkbar*n*n*n*n
     i = self.g*(v + 77.)
     rhs[self.nodeindex] -= i
     #print "leave current ", rhs
 
-  def conductance(self, d, unused):
+  def conductance(self, d):
     #print "enter conductance ", d
     d[self.nodeindex] += self.g
     #print "leave conductane ", d, rhs
 
-  def fixed_step_solve(self, unused1, unused2):
+  def fixed_step_solve(self, dt):
     v = self.seg.v
     n = self.n
     ninf, ninv = self.ninftau(v)
-    dt = h.dt
     self.n += (1. - exp(-dt*ninv))*(ninf - n)
-    #print 'fixed step solve v=%g ninf=%g n=%g leave n=%g\n'%(v, ninv, n, self.n)
+    #print 'fixed step solve dt=%g v=%g ninf=%g n=%g leave n=%g\n'%(dt, v, ninv, n, self.n)
 
   def ode_count(self, offset):
-    print 'just set offset to', offset
+    #print 'ode_count offset ', offset
     self.offset = offset
     return 1
 
-  def ode_reinit(self, y, unused):
+  def ode_reinit(self, y):
     y[self.offset] = self.n
 
-  def ode_fun(self, y, ydot): # from y determine ydot
+  def ode_fun(self, t, y, ydot): # from y determine ydot (if ydot is not None)
     v = self.seg.v
     self.n = y[self.offset]
-    print "ode_fun t=%g v=%g n=%g\n"%(h.t, v, self.n)
+    #print "ode_fun t=%g v=%g n=%g"%(h.t, v, self.n)
+    if ydot == None:
+      return
     ninf, ninv = self.ninftau(v)
     ydot[self.offset] = (ninf - self.n)*ninv
 
-  def ode_solve(self, b, y): #solve mx=b replace b with x (y available if m depends on it
+  def ode_solve(self, dt, b, y): #solve mx=b replace b with x (y available if m depends on it
     v =self.seg.v
-    dt = h.dt
     x,ninv = self.ninftau(v)
     b[self.offset] /= 1. + dt*ninv
+    #print 'ode_solve dt=%g'% dt
 
   def ninftau(self, v):
     na = self.nalpha(v)
@@ -91,19 +93,45 @@ class kchan():
     else:
       return x/(exp(x) - 1.)
 
-s = h.Section()
-s.nseg = 1
-s.diam = 10
-s.L = 100/(h.PI*s.diam)
-s.insert('hh')
+def mk():
+  global s, stim
+  s = h.Section()
+  s.nseg = 1
+  s.diam = 10
+  s.L = 100/(h.PI*s.diam)
+  s.insert('hh')
+  h.usetable_hh = 0
+  stim = h.IClamp(s(0.5))
+  stim.delay = 0.5
+  stim.dur = 0.1
+  stim.amp = 0.3
+mk()
+
+vhh = h.Vector()
+vhh.record(s(.5)._ref_v)
+h.run()
+vhhfixed = vhh.c()
+h.cvode_active(1)
+h.run()
+vhhcvode = vhh.c()
+
+h.cvode_active(0)
 s.gkbar_hh = 0
+s.nseg=3
+s.nseg=1
 k = kchan(s(.5))
-stim = h.IClamp(s(0.5))
-stim.delay = 0.5
-stim.dur = 0.1
-stim.amp = 0.3
-h.finitialize(-65)
-h.fadvance()
+
+h.run()
+print "mean sq error for fixed step ", vhh.meansqerr(vhhfixed)
+
+h.cvode_active(1)
+h.run()
+print "trajectory sizes for cvode ", vhhcvode.size(), vhh.size()
+s.gkbar_hh = .036
+k.gkbar = 0.0
+h.run()
+print "trajectory sizes for cvode ", vhhcvode.size(), vhh.size()
+
 
 #h.run()
 '''
@@ -115,3 +143,5 @@ h.finitialize(0)
 print "cvode fadvance"
 h.fadvance()
 '''
+
+h.load_file("temp.ses")
