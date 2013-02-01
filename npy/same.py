f97f9eb62e9fed342099bfad854354b0fbff43f6
Michael Hines <michael.hines@yale.edu>
2013-02-01 09:57:05 -0500

    Python section and hoc object 'same(o)' if they wrap the same internal Section or Object.

---
+++ npy/same.py
@@ -0,0 +1,16 @@
+from neuron import h
+
+a = h.Section(name='bob')
+b = h.Section(name='bob')
+print (a.same(b), a is b) == (False, False)
+c = h.cas()
+print (c.same(a), c.same(b), c is a, c is b) == (True, False, True, False)
+h('create s, d')
+
+print (h.s.same(h.s), h.s is h.s) == (True, False)
+
+print h.s.same(5) == False
+
+h('objref vv')
+h('vv = new Vector(3)')
+print (h.vv == h.vv, h.vv.same(h.vv)) == (False, True)
