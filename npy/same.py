39583ad26870cedff05833b80d28c96227169910
Michael Hines <michael.hines@yale.edu>
2013-02-01 14:34:18 -0500

    == also works for nrn.Section

--- npy/same.py
+++ npy/same.py
@@ -2,14 +2,15 @@ from neuron import h
 
 a = h.Section(name='bob')
 b = h.Section(name='bob')
-print (a.same(b), a is b) == (False, False)
+print (a.same(b), a is b, a == b) == (False, False, False)
 c = h.cas()
-print (c.same(a), c.same(b), c is a, c is b) == (True, False, True, False)
+print (c.same(a), c == a, c.same(b), c is a, c is b) == (True, True, False, True, False)
 h('create s, d')
 
-print (h.s.same(h.s), h.s is h.s) == (True, False)
+print (h.s.same(h.s), h.s is h.s, h.s == h.s) == (True, False, True)
 
 print h.s.same(5) == False
+print (h.s == 5) == False
 
 h('objref vv')
 h('vv = new Vector(3)')
