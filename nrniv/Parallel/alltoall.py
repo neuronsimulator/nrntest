6ecd840a706bce60abc0441412982de4c16ceace
Michael Hines <michael.hines@yale.edu>
2013-04-29 14:17:48 -0400

    alltoall test

---
+++ nrniv/Parallel/alltoall.py
@@ -0,0 +1,47 @@
+from neuron import h
+pc = h.ParallelContext()
+nhost = int(pc.nhost())
+rank = int(pc.id())
+
+#Keep host output from being intermingled.
+#Not always completely successful.
+import sys
+def serialize():
+  for r in range(nhost):
+    pc.barrier()
+    if r == rank:
+      yield r
+  pc.barrier()
+
+data = [(rank, i) for i in range(nhost)]
+
+for r in serialize():
+  if r==0:
+    f = open('temp', 'w')
+    f.write('source data\n')
+    f.flush()
+    f.close()
+  f = open('temp', 'a')
+  f.write(str(rank)+str(data)+'\n')
+  f.flush()
+
+data = pc.py_alltoall(data)
+
+for r in serialize():
+  if r==0:
+    f = open('temp', 'a')
+    f.write('destination data\n')
+    f.flush()
+    f.close()
+  f = open('temp', 'a')
+  f.write(str(rank)+str(data)+'\n')
+  f.flush()
+
+pc.runworker()
+
+from os import system
+print system('cmp temp alltoall.cmp')
+
+pc.done()
+h.quit()
+
