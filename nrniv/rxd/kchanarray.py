a8dc21949ed2f30683ed84b4f2299397fdd4a250
Michael Hines <michael.hines@yale.edu>
2013-10-01 06:52:39 -0400

    working example of nonvint_block_supervisor for array of channels

--- nrniv/rxd/kchanarray.py
+++ nrniv/rxd/kchanarray.py
@@ -1,7 +1,7 @@
 from neuron import h
 import numpy
 from neuron import nonvint_block_supervisor as nbs
-from math import exp
+from numpy import exp
 
 h.load_file('nrngui.hoc')
 
@@ -13,8 +13,13 @@ missing in ik.
 '''
 
 class kchan():
-  def __init__(self, seg):
-    self.seg = seg #dangerous as contents can disappear when nseg changes
+  def __init__(self, sectionlist=None):
+    if sectionlist:
+      self.sl = sectionlist
+    else:
+      self.sl = h.SectionList()
+      for sec in h.allsec():
+        self.sl.append()
     self.call = [self.setup, self.initialize,
       self.current, self.conductance, self.fixed_step_solve,
       self.ode_count, self.ode_reinit, self.ode_fun, self.ode_solve, None,
@@ -27,55 +32,74 @@ class kchan():
     nbs.unregister(self.call)
 
   def setup(self):
-    self.nodeindex = self.seg.node_index()
+    size = 0
+    for sec in self.sl:
+      for seg in sec:
+        size += 1
+    self.pv = h.PtrVector(size)
+    self.hv = h.Vector(size)
+    self.v = self.hv.as_numpy()
+    self.nodeindices = numpy.empty(size, dtype=int)
+    self.n = numpy.empty(size, dtype=float)
+    self.g = numpy.empty(size, dtype=float)
+    i=0
+    for sec in self.sl:
+      for seg in sec:
+        self.pv.pset(i, seg._ref_v)
+        self.nodeindices[i] = seg.node_index()
+        i += 1
     #print "setup ", self.nodeindex
 
   def initialize(self):
-    self.n, x = self.ninftau(self.seg.v)
+    self.pv.gather(self.hv)
+    self.n, x = self.ninftau(self.v)
     #print "initialize v=%g n=%g"% (self.seg.v, self.n)
 
   def current(self, rhs):
     #print "enter current ", rhs
-    v = self.seg.v
+    self.pv.gather(self.hv)
+    v = self.v
     n = self.n
     self.g = self.gkbar*n*n*n*n
     i = self.g*(v + 77.)
-    rhs[self.nodeindex] -= i
+    rhs[self.nodeindices] -= i
     #print "leave current ", rhs
 
   def conductance(self, d):
     #print "enter conductance ", d
-    d[self.nodeindex] += self.g
+    d[self.nodeindices] += self.g
     #print "leave conductane ", d, rhs
 
   def fixed_step_solve(self, dt):
-    v = self.seg.v
+    self.pv.gather(self.hv)
     n = self.n
-    ninf, ninv = self.ninftau(v)
+    ninf, ninv = self.ninftau(self.v)
     self.n += (1. - exp(-dt*ninv))*(ninf - n)
     #print 'fixed step solve dt=%g v=%g ninf=%g n=%g leave n=%g\n'%(dt, v, ninv, n, self.n)
 
   def ode_count(self, offset):
     #print 'ode_count offset ', offset
     self.offset = offset
-    return 1
+    return len(self.v)
 
   def ode_reinit(self, y):
-    y[self.offset] = self.n
+    y[self.offset:self.offset+len(self.n)] = self.n
 
   def ode_fun(self, t, y, ydot): # from y determine ydot (if ydot is not None)
-    v = self.seg.v
-    self.n = y[self.offset]
+    last = self.offset + len(self.n)
+    self.pv.gather(self.hv)
+    self.n = y[self.offset:last]
     #print "ode_fun t=%g v=%g n=%g"%(h.t, v, self.n)
     if ydot == None:
       return
-    ninf, ninv = self.ninftau(v)
-    ydot[self.offset] = (ninf - self.n)*ninv
+    ninf, ninv = self.ninftau(self.v)
+    ydot[self.offset:last] = (ninf - self.n)*ninv
 
   def ode_solve(self, dt, t, b, y): #solve mx=b replace b with x (y available if m depends on it
-    v =self.seg.v
-    x,ninv = self.ninftau(v)
-    b[self.offset] /= 1. + dt*ninv
+    last = self.offset + len(self.n)
+    self.pv.gather(self.hv)
+    x,ninv = self.ninftau(self.v)
+    b[self.offset:last] /= 1. + dt*ninv
     #print 'ode_solve dt=%g'% dt
 
   def ninftau(self, v):
@@ -91,44 +115,33 @@ class kchan():
     return 0.125*exp(-(v+65.)/80.)
 
   def einstein1(self, x):
-    if abs(x) < 1e-6:
-      return 1. - x/2.
-    else:
-      return x/(exp(x) - 1.)
+    return x/(exp(x) - 1.)
+    #return numpy.piecewise(x, [x > 1e-6 or x < -1e-6], \
+    #  [lambda x: x / (exp(x) - 1.) , lambda x: 1. - x/2.])
 
 def mk():
   global s, stim
   s = h.Section()
-  s.nseg = 1
-  s.diam = 10
-  s.L = 100/(h.PI*s.diam)
+  s.nseg = 10000
+  s.diam = 1
+  s.L = 20000
   s.insert('hh')
   h.usetable_hh = 0
-  stim = h.IClamp(s(0.5))
+  stim = h.IClamp(s(0))
   stim.delay = 0.5
   stim.dur = 0.1
-  stim.amp = 0.3
+  stim.amp = 3
 mk()
 
-vhh = h.Vector()
-vhh.record(s(.5)._ref_v)
-h.run()
-vhhfixed = vhh.c()
-h.cvode_active(1)
-h.run()
-vhhcvode = vhh.c()
+#h.load_file("temp.ses")
 
-h.cvode_active(0)
+def prun():
+  xx = h.startsw()
+  h.run()
+  print h.startsw() - xx
+
+prun()
+
 s.gkbar_hh = 0
-k = kchan(s(.5))
-
-h.run()
-print "mean sq error for fixed step ", vhh.meansqerr(vhhfixed)
-
-h.cvode_active(1)
-h.run()
-print "trajectory sizes for cvode ", vhhcvode.size(), vhh.size()
-s.gkbar_hh = .036
-k.gkbar = 0.0
-h.run()
-print "trajectory sizes for cvode ", vhhcvode.size(), vhh.size()
+k = kchan()
+prun()
