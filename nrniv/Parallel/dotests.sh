4968bcede12fc15b6e5633563525a8f49822c08c
Nicolas Cornu <nicolac76@yahoo.fr>
2021-09-13 17:49:17 +0200

    More fixes in scripts

--- nrniv/Parallel/dotests.sh
+++ nrniv/Parallel/dotests.sh
@@ -2,13 +2,13 @@
 
 err=0
 
-files="alltoall.py alltoall_dict.py collective.py"
+files=("alltoall.py" "alltoall_dict.py" "collective.py")
 
 echo $files $np
 
 function f() {
   echo "hello $1"
-  a=`mpiexec --oversubscribe -n 4 nrniv -nobanner -mpi -python $1 2>/dev/null | sed -n '$p'`
+  a=$(mpiexec --oversubscribe -n 4 nrniv -nobanner -mpi -python $1 2>/dev/null | sed -n '$p')
   if test "$a" != 0 ; then
     echo "$1 failed"
     err=1
@@ -26,6 +26,6 @@ done
 cd pargap
 nrnivmodl
 i=msgap.py
-a=`mpiexec -n 2 nrniv -nobanner -mpi -python $i |sed -n '$p'`
+a=$(mpiexec -n 2 nrniv -nobanner -mpi -python $i |sed -n '$p')
 f
 
