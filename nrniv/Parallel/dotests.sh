e3c895aca1377773ca01e8f7f4604c90fd1ab6e1
Alexandru Săvulescu <alexandru.savulescu@epfl.ch>
2021-09-13 16:09:59 +0200

    nrnversion doesn't rely on autotools anymore
    * nrniv/Parallel/dotests.sh always oversubscribe

--- nrniv/Parallel/dotests.sh
+++ nrniv/Parallel/dotests.sh
@@ -4,17 +4,11 @@ err=0
 
 files="alltoall.py alltoall_dict.py collective.py"
 
-arg=""
-osname=`uname -s`
-if test "$osname" = "Darwin" ; then
-  arg="--oversubscribe"
-fi
-
 echo $files $np
 
 function f() {
   echo "hello $1"
-  a=`mpiexec $arg -n 4 nrniv -nobanner -mpi -python $1 2>/dev/null | sed -n '$p'`
+  a=`mpiexec --oversubscribe -n 4 nrniv -nobanner -mpi -python $1 2>/dev/null | sed -n '$p'`
   if test "$a" != 0 ; then
     echo "$1 failed"
     err=1
