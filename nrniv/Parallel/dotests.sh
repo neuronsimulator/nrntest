a6e8b4daf33ed595ae5aceb7a7e6693f8b1f2190
Alexandru Săvulescu <alexandru.savulescu@epfl.ch>
2021-10-06 23:14:15 +0200

    drop alltoall_dict.py test + allow oversubscribe to be overwritten  (#18)
    * allow oversubscribe to be overwritten
      * needed for mpich testing via nrn-build-ci
    * drop alltoall_dict.py testing
      * rely on basic test from NRN

--- nrniv/Parallel/dotests.sh
+++ nrniv/Parallel/dotests.sh
@@ -2,12 +2,12 @@
 
 err=0
 
-files=("alltoall.py" "alltoall_dict.py" "collective.py" "pargap/msgap.py")
+files=("alltoall.py" "collective.py" "pargap/msgap.py")
 nrnivmodl pargap
 
 function f() {
   echo "Running nrniv/Parallel/${1}"
-  a=$(mpiexec --oversubscribe -n 4 nrniv -nobanner -mpi -python $1 2>/dev/null | sed -n '$p')
+  a=$(mpiexec ${MPIEXEC_OVERSUBSCRIBE---oversubscribe} -n 4 nrniv -nobanner -mpi -python $1 2>/dev/null | sed -n '$p')
   echo "${1} stdout: ${a}"
   if test "$a" != 0 ; then
     echo "$1 failed"
