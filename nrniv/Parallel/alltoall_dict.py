12df7acc69a354d5d395680a79096b16c3f0d56d
Alexandru Săvulescu <alexandru.savulescu@epfl.ch>
2021-10-06 13:35:53 +0200

    Improve testing in nrniv/Parallel  (#17)
    * fix dotests.sh in order to run all scripts
    * run 2to3 on python scripts
    * run `black`

--- nrniv/Parallel/alltoall_dict.py
+++ nrniv/Parallel/alltoall_dict.py
@@ -1,20 +1,27 @@
-#pc.py_alltoall(dict) is an error
+# pc.py_alltoall(dict) is an error
 from neuron import h
+
 pc = h.ParallelContext()
 nhost = int(pc.nhost())
 rank = int(pc.id())
 err = 1
 
-src = {i:(100+i) for i in range(nhost)}
-print src
+# A nice improvement to pc.py_alltoall() is that it accept a dict as an arg.
+if nhost > 1:
+    if rank == 0:
+        print(0)
+    pc.barrier()
+    quit()
+
+src = {i: (100 + i) for i in range(nhost)}
+# print(src)
 
 try:
-  dest = pc.py_alltoall(src)
-  err = 1
+    dest = pc.py_alltoall(src)
+    err = 1
 except:
-  err = 0
+    err = 0
 
 err = int(pc.allreduce(err, 1))
-print err
+print(err)
 h.quit()
-
--- nrniv/Parallel/pargap/msgap.py
+++ nrniv/Parallel/pargap/msgap.py
@@ -6,13 +6,15 @@
 # run this test with 2 ranks
 
 from neuron import h
+
 pc = h.ParallelContext()
 rank = int(pc.id())
 nhost = int(pc.nhost())
 h.load_file("stdrun.hoc")
 
-#two section cell type that can be easily multisplit
-h('''
+# two section cell type that can be easily multisplit
+h(
+    """
 
 begintemplate Cell
 public soma, dend
@@ -25,49 +27,56 @@ proc init() {
 }
 endtemplate Cell
 
-''')
+"""
+)
 
 # 1 cell for each rank
 cell = h.Cell()
 
 # rank 0 cell uses multisplit. Works if rank = 1
 if rank == 0:
-  h.disconnect(sec=cell.dend)
-  pc.multisplit(0.0, rank, sec=cell.dend)
-  pc.multisplit(1.0, rank, sec=cell.soma)
+    h.disconnect(sec=cell.dend)
+    pc.multisplit(0.0, rank, sec=cell.dend)
+    pc.multisplit(1.0, rank, sec=cell.soma)
 
 # gap junction between soma of rank i and rank (i+1)%nhost
 # use sids of 2*i and 2*i + 1 for the source voltages
 halfgaps = []
+
+
 def mkgap(i):
-  if rank == i:
-    gap = h.HalfGap(cell.soma(.5))
-    halfgaps.append(gap)
-    pc.source_var(cell.soma(.5)._ref_v, i, sec=cell.soma)
-    gap = pc.target_var(gap, gap._ref_vgap, i+1)
-  if rank == (i+1)%nhost:
-    gap = h.HalfGap(cell.soma(.5))
-    halfgaps.append(gap)
-    pc.source_var(cell.soma(.5)._ref_v, i+1, sec=cell.soma)
-    gap = pc.target_var(gap, gap._ref_vgap, i)
+    if rank == i:
+        gap = h.HalfGap(cell.soma(0.5))
+        halfgaps.append(gap)
+        pc.source_var(cell.soma(0.5)._ref_v, i, sec=cell.soma)
+        gap = pc.target_var(gap, gap._ref_vgap, i + 1)
+    if rank == (i + 1) % nhost:
+        gap = h.HalfGap(cell.soma(0.5))
+        halfgaps.append(gap)
+        pc.source_var(cell.soma(0.5)._ref_v, i + 1, sec=cell.soma)
+        gap = pc.target_var(gap, gap._ref_vgap, i)
+
 
 mkgap(0)
 
+
 def callback():
-  print 't=', h.t
+    print("t=", h.t)
+
 
 def prun():
-  pc.timeout(1)
-  pc.multisplit()
-  pc.set_maxstep(1)
-  pc.setup_transfer()
-  h.stdinit()
-  if rank == 0: h.cvode.event(2.001, callback) #works when 2.0
-  pc.psolve(5)
+    pc.timeout(1)
+    pc.multisplit()
+    pc.set_maxstep(1)
+    pc.setup_transfer()
+    h.stdinit()
+    if rank == 0:
+        h.cvode.event(2.001, callback)  # works when 2.0
+    pc.psolve(5)
 
-if __name__ == '__main__':
-  prun()
-  pc.barrier()
-  print 0
-  h.quit()
 
+if __name__ == "__main__":
+    prun()
+    pc.barrier()
+    print(0)
+    h.quit()
