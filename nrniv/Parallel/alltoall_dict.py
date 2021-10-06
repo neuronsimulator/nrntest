174c791ddb0a494af0e7b91c77717989646504f1
Alexandru Săvulescu <alexandru.savulescu@epfl.ch>
2021-10-06 13:10:12 +0200

    address cr

--- nrniv/Parallel/alltoall_dict.py
+++ nrniv/Parallel/alltoall_dict.py
@@ -5,14 +5,21 @@ nhost = int(pc.nhost())
 rank = int(pc.id())
 err = 1
 
-src = [(i,(100+i)) for i in range(nhost)]
+# A nice improvement to pc.py_alltoall() is that it accept a dict as an arg.
+if pc.nhost() > 1:
+  if pc.id() == 0:
+    print((0))
+  pc.barrier()
+  quit()
+
+src = {i:(100+i) for i in range(nhost)}
 #print(src)
 
 try:
   dest = pc.py_alltoall(src)
-  err = 0
-except:
   err = 1
+except:
+  err = 0
 
 err = int(pc.allreduce(err, 1))
 print(err)
