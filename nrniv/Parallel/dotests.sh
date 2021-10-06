4ad2f70e88aefb743096db7a465bbd61a47a1a3d
Alexandru Săvulescu <alexandru.savulescu@epfl.ch>
2021-10-06 15:49:37 +0200

    allow oversubscribe to be overwritten
    * needed for mpich testing via nrn-build-ci

--- nrniv/Parallel/dotests.sh
+++ nrniv/Parallel/dotests.sh
@@ -7,7 +7,7 @@ nrnivmodl pargap
 
 function f() {
   echo "Running nrniv/Parallel/${1}"
-  a=$(mpiexec --oversubscribe -n 4 nrniv -nobanner -mpi -python $1 2>/dev/null | sed -n '$p')
+  a=$(mpiexec ${MPIEXEC_OVERSUBSCRIBE---oversubscribe} -n 4 nrniv -nobanner -mpi -python $1 2>/dev/null | sed -n '$p')
   echo "${1} stdout: ${a}"
   if test "$a" != 0 ; then
     echo "$1 failed"
