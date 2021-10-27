933d1c36829be507f73d418862625b574c40e1a0
Alexandru Săvulescu <alexandru.savulescu@epfl.ch>
2021-10-27 13:06:06 +0200

    enable more parallel testing
    * make `nrniv/Parallel/recv` CI enabled

--- nrniv/Parallel/recv/test.sh
+++ nrniv/Parallel/recv/test.sh
@@ -1,23 +1,44 @@
-#!/bin/sh
+#!/bin/bash
 
-NRN="$HOME/neuron/nrnmpi/x86_64/bin/nrniv"
+nrnivmodl
+
+ERR=0
 
 run() {
-	rm -f temp1 temp2 temp3 temp4 recv.dat out.dat
+	rm -f recv$1.dat* out$1.dat* recv.dat out.dat
 	ct="celltype=$1"
-	echo "$ct $2"
-	$NRN -nobanner -c "$ct" -c "{$2}" init.hoc > /dev/null
-	./sortrecv recv.dat temp1
-	cmp temp1 recv$1.std
-	sortspike out.dat temp2
-	cmp temp2 out$1.std
+	echo "run with -> $ct $2 <-"
+	nrniv -nobanner -nogui -c "$ct" -c "{$2}" init.hoc > /dev/null
+	./sortrecv recv.dat recv$1.dat.sorted
+	cmp recv$1.dat.sorted recv$1.std
+	if [ $? -ne 0 ]; then
+		ERR=1
+	fi
+	sortspike out.dat out$1.dat.sorted
+	cmp out$1.dat.sorted out$1.std
+	if [ $? -ne 0 ]; then
+		ERR=1
+	fi
 	rm -f recv.dat out.dat
-	echo "mpi $ct $2"
-	mpiexec -n 4 $NRN -mpi -nobanner -c "$ct" -c "{$2}" init.hoc > /dev/null
-	./sortrecv recv.dat temp3
-	cmp temp3 temp1
-	sortspike out.dat temp4
-	cmp temp4 temp2
+
+	RUN_WITH_MPI="${have_nrnmpi-yes}"
+	if [ "${RUN_WITH_MPI}" = "yes" ] ; then
+		echo "run with mpi -> $ct $2 <-"
+		mpiexec ${MPIEXEC_OVERSUBSCRIBE---oversubscribe} -n 4 nrniv -mpi -nobanner -nogui -c "$ct" -c "{$2}" init.hoc > /dev/null
+		./sortrecv recv.dat recv$1.dat.sorted.mpi
+		cmp recv$1.dat.sorted.mpi recv$1.dat.sorted
+		if [ $? -ne 0 ]; then
+			ERR=1
+		fi
+		sortspike out.dat out$1.dat.sorted.mpi
+		cmp out$1.dat.sorted.mpi out$1.dat.sorted
+		if [ $? -ne 0 ]; then
+			ERR=1
+		fi
+	else
+		echo "run with mpi is DISABLED-> $ct $2 <-"
+	fi
+	
 }
 
 run 1 ""
@@ -34,4 +55,4 @@ run 0 "binqueue=1"
 run 0 "spkbufsize=1 binqueue=1"
 run 0 "spkbufsize=1 binqueue=1 selfqueue=1"
 
-
+exit $ERR
