d969746380ae0176e06de4ab079007c285fd69c2
Goran Jelic-Cizmek <goran.jelic-cizmek@epfl.ch>
2024-11-06 17:39:58 +0100

    Another script refactored

--- nrniv/Parallel/dotests.sh
+++ nrniv/Parallel/dotests.sh
@@ -1,25 +1,27 @@
 #!/usr/bin/env bash
 
+set -u
+
 err=0
 
 files=("alltoall.py" "collective.py" "pargap/msgap.py")
 nrnivmodl pargap
 
-function f() {
-  echo "Running nrniv/Parallel/${1}"
-  a=$(mpiexec ${MPIEXEC_OVERSUBSCRIBE---oversubscribe} -n 4 nrniv -nobanner -mpi -python $1 2>/dev/null | sed -n '$p')
-  echo "${1} stdout: ${a}"
-  if test "$a" != 0 ; then
-    echo "$1 failed"
-    err=1
-  else
-    echo "$1 succeeded"
-  fi
-  
+run_neuron() {
+    echo "Running nrniv/Parallel/${1}"
+    output="$(mpiexec ${MPIEXEC_OVERSUBSCRIBE---oversubscribe} -n 4 nrniv -nobanner -mpi -python "$1" 2>/dev/null | sed -n '$p')"
+    echo "${1} stdout: ${output}"
+    if test "${output}" != 0; then
+        echo "$1 failed"
+        err=1
+    else
+        echo "$1 succeeded"
+    fi
+
 }
 
-for i in ${files[@]}; do
- f $i
+for path in ${files[@]}; do
+    run_neuron "${path}"
 done
-exit $err
 
+exit $err
